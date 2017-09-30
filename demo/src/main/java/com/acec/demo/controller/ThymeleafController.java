ea618e7118ad49b399ec7c58d8f74c1c765b06a7
qlr-fujindong <fujd@qilerong.com>
2017-09-30 11:49:10 +0800

    提前还款列表可以展示还款明细

--- demo/src/main/java/com/acec/demo/controller/ThymeleafController.java
+++ demo/src/main/java/com/acec/demo/controller/ThymeleafController.java
@@ -6,8 +6,12 @@
 package com.acec.demo.controller;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +32,24 @@ public class ThymeleafController {
         String formattedDate = dateFormat.format(date);
         model.addAttribute("currentTime", formattedDate);
 
+        List ls = new ArrayList();
+        Map map = new HashMap();
+        map.put("id", "id1111");
+        map.put("webLogo", "Logo1111");
+        map.put("url", "http://www.sina.com.cn1111");
+        map.put("title", "标题1111");
+        map.put("description", "这个一个备注1111");
+        ls.add(map);
+        
+        Map map2 = new HashMap();
+        map2.put("id", "id2222");
+        map2.put("webLogo", "Logo2222");
+        map2.put("url", "http://www.sina.com.cn2222");
+        map2.put("title", "标题2222");
+        map2.put("description", "这个一个备注2222");
+        ls.add(map2);
+        
+        model.addAttribute("collects", ls);
         return "hello";
     }
 }
